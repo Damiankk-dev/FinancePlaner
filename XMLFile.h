a5aec4f4e40b44820724a936391ad411e1dd8891
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-04-14 17:01:19 +0200

    renamed XMLFile

---
+++ XMLFile.h
@@ -0,0 +1,24 @@
+#ifndef XML_FILE
+#define XML_FILE
+
+#include <iostream>
+
+#include "Markup.h"
+
+class XMLFile{
+
+const std::string FILENAME;
+
+public:
+    XMLFile() : FILENAME("defaultFilename.xml"){}
+    XMLFile(std::string filename) : FILENAME(filename){}
+    bool isFileInitializedProperly();
+    std::string getFilename();
+private:
+    void initializeFile();
+
+};
+
+
+
+#endif // XML_FILE
