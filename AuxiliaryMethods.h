93e2bb7e3299ba57f26b7eb60434b6a43d6fe53b
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-04-20 18:18:23 +0200

    eh...as above

--- AuxiliaryMethods.h
+++ AuxiliaryMethods.h
@@ -5,9 +5,21 @@
 #include<ctime>
 
 class AuxiliaryMethods{
+public:
     static char readChar();
+
+    static int convertString2Int();
+    static int readInteger();
+
+    static float convertString2Float();
+
     static std::string convertDate2String();
+    static std::string convertInt2String();
+    static std::string convert2String();
+    static std::string changeCommaToPeriod();
+
     static time_t convertString2Date();
+
 };
 
 #endif // AUXILIARY_METHODS_H
