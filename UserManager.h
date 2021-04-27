f8ddfd39ab7133231529546794a312c7b9c47d38
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-04-27 17:10:33 +0200

    Declaration of methods from plan, created constructor

--- UserManager.h
+++ UserManager.h
@@ -3,11 +3,35 @@
 
 #include <iostream>
 
+#include "User.h"
 #include "AuxiliaryMethods.h"
 #include "RegisteredUsersFile.h"
 
 class UserManager{
 public:
-    UserManager();
+    UserManager(std::string registeredUsersFilename) : registeredUsersFile(registeredUsersFilename) {
+        users = registeredUsersFile.loadUsersFromFile();
+    };
+
+    void registerUser();
+    void changeUserPassword();
+    void signOutUser();
+
+    bool isUserSignedIn();
+
+    int signInUser();
+    int getSignedInUserID();
+
+private:
+    int signedInUserID;
+    vector<User> users;
+    RegisteredUsersFile registeredUsersFile;
+
+    bool isUsernameExists(std::string);
+    bool isPasswordCorrect();//optional
+
+    int getNewUserID();
+
+    User addNewUser();
 };
 #endif // USER_MANAGER_H
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "RegisteredUsersFile.h"
 #include "User.h"
 #include "AuxiliaryMethods.h"
+#include "UserManager.h"
 
 #include <vector>
 
@@ -16,6 +17,7 @@ int main()
 //    cout << "read sign is : " << AuxiliaryMethods::readChar() << endl;
 //    cout << "Dodam 1111 do 2222\n" << AuxiliaryMethods::convertString2Int("1111") + AuxiliaryMethods::convertString2Int("2222");
     cout << "Float: " << endl;
+    UserManager newUserManager("juzerzy.xml");
     cout << AuxiliaryMethods::convertString2Date("1978-03-15") << endl;
     cout << AuxiliaryMethods::convertDate2String(AuxiliaryMethods::convertString2Date("1978-03-15"))<< endl;
 //    string s1 = "123,43";
