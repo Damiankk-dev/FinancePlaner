1cd9fdffbcb62cbe0c84e7dfb9517c09290fb42d
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-04-27 16:59:49 +0200

    Initialization of files of UserManager class

---
+++ UserManager.h
@@ -0,0 +1,13 @@
+#ifndef USER_MANAGER_H
+#define USER_MANAGER_H
+
+#include <iostream>
+
+#include "AuxiliaryMethods.h"
+#include "RegisteredUsersFile.h"
+
+class UserManager{
+public:
+    UserManager();
+};
+#endif // USER_MANAGER_H
