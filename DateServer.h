ea61e349d4e2f77fc4bdd4684bcf85b530e3fa5d
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-04-29 17:25:55 +0200

    method declarations

--- DateServer.h
+++ DateServer.h
@@ -1,9 +1,19 @@
 #ifndef DATE_SERVER_H
 #define DATE_SERVER_H
 
+#include <iostream>
+
 class DateServer{
 public:
-    DateServer();
+    std::string getDateInProperFormat();
+
+    bool isDateCorrect();
+    bool isDateAfter2000();
+    bool isDateFormatProper();
+
+    std::string getTodayDate();
+    std::string readSystemDate();
+    std::string isDateBefereEndOfCurrentMonth();
 };
 
 #endif // DATE_SERVER_H
