370750af4458be7d3f69e56593903f83932c8bd9
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-05 18:09:37 +0200

    Initialized files for CashflowCreator class

---
+++ CashflowCreator.cpp
@@ -0,0 +1,1 @@
+#include "CashflowCreator.h"
---
+++ CashflowCreator.h
@@ -0,0 +1,15 @@
+#ifndef CASHFLOW_CREATOR_H
+#define CASHFLOW_CREATOR_H
+
+#include <iostream>
+
+#include "AuxiliaryMethods.h"
+#include "Cashflow.h"
+
+class CashflowCreator{
+public:
+    CashflowCreator();
+};
+
+
+#endif // CASHFLOW_CREATOR_H
