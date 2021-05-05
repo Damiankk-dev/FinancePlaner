ee9411d4e69a953699386822efd841ce80dda0aa
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-05 17:58:57 +0200

    Created and tested first stable version of Cashflow struct

--- Cashflow.h
+++ Cashflow.h
@@ -6,6 +6,27 @@
 #include "DateServer.h"
 
 class Cashflow{
+    const int SIGNED_USER_ID;
+    int id;
+    double value;
+    std::string label;
+    std::string date;
+public:
+    Cashflow(int signedUserID, int id = 0, double value = 0.0, std::string label = "", std::string date = ""):SIGNED_USER_ID(signedUserID){
+        this->id = id;
+        this->value = value;
+        this->label = label;
+        this->date = date;
+    }
+    void setId(int newId);
+    void setValue(double newValue);
+    void setLabel(std::string newLabel);
+    void setDate(std::string newDate);
+
+    int getId();
+    double getValue();
+    std::string getLabel();
+    std::string getDate();
 
 };
 #endif // CASHFLOW_H
