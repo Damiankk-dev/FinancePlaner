14f0ddb0cf41eb2d63958077a5814585caddcac7
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-07 16:32:20 +0200

    Changed order of elements saving into file

--- CashflowFile.cpp
+++ CashflowFile.cpp
@@ -1,14 +1,28 @@
 #include "CashflowFile.h"
 
-int CashflowFile::getLastCashflowID(){
+int CashflowFile::getLastCashflowID(int signedInUserId){
+    CashflowFile *loadedFile = this;
+    loadedFile->Load(loadedFile->getFilename() );
+    loadedFile->resetPositionAndMoveIntoRoot();
+    while (loadedFile->FindElem( "USER_ID" ) ){
+//        loadedFile->IntoElem();
+        std::cout << loadedFile->GetData() << std::endl;
+//            if (stoi ( this->GetData() ) == signedInUserId ){
+//                std::cout << "cukces\n";
+//            }
+//        this->isFileEmpty = false;
+//        loadedFile->IntoElem();
+//        loadedUsers.push_back(getUserData() );
+//        loadedFile->OutOfElem();
+    }
+    return 0;
 
 }
 void CashflowFile::appendCasflowToFile(Cashflow* cashflow){
     this->resetPositionAndMoveIntoRoot();
-    this->AddElem( "USER_ID" , cashflow->getSignedInUserId() );
-    this->IntoElem();
     this->AddElem( "CASHFLOW" );
     this->IntoElem();
+    this->AddElem( "USER_ID" , cashflow->getSignedInUserId() );
     this->AddElem(  "ID", cashflow->getId() );
     this->AddElem( "VALUE", cashflow->getValue() );
     this->AddElem( "LABEL", cashflow->getLabel() );
