09906db3da521e110e26fcbe2a0e12575cc178f3
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-12 16:33:11 +0200

    Prepared implementations of methods

--- BalanceCreator.cpp
+++ BalanceCreator.cpp
@@ -1,1 +1,12 @@
 #include "BalanceCreator.h"
+
+//void BalanceCreator::showBalanceByGivenPeriod();
+//std::string BalanceCreator::setPeriod();
+//void BalanceCreator::sortBalanceByDateOldest();
+//void BalanceCreator::showCashflowTable();
+//
+//float BalanceCreator::calculateBalance();
+//float BalanceCreator::calculateIncome();
+//float BalanceCreator::calculateExpense();
+//
+//std::vector<Cashflow> BalanceCreator::loadCasfhlowsFromFileByGivenPeriod();
