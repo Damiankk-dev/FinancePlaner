ff9f61137ca5c108f10dd7eb549aa7beecf7f691
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-12 16:54:42 +0200

    Implementation of specifyPeriod method, tested

--- PeriodServer.cpp
+++ PeriodServer.cpp
@@ -1,5 +1,13 @@
 #include "PeriodServer.h"
 
 std::string PeriodServer::specifyPeriod(){
+    std::string specifiedPeriod = "";
+    std::cout << "Okresl daty okresu,\nz ktorego chcesz policzyc bilans\n";
 
+    std::cout << "Podaj dolna granice okresu\n";
+    specifiedPeriod = getDateInProperFormat();
+    std::cout << "Podaj gorna granice okresu\n";
+    specifiedPeriod += "|";
+    specifiedPeriod += getDateInProperFormat();
+    return specifiedPeriod;
 }
