62ef73e65187dbef3ba0c5aa8a13935c7068bd3a
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-12 16:21:57 +0200

    initialization of BalanceCreator files

---
+++ BalanceCreator.cpp
@@ -0,0 +1,1 @@
+#include "BalanceCreator.h"
---
+++ BalanceCreator.h
@@ -0,0 +1,11 @@
+#ifndef BALANCE_CREATOR_H
+#define BALANCE_CREATOR_H
+
+#include <iostream>
+
+class BalanceCreator{
+public:
+
+};
+
+#endif // BALANCE_CREATOR_H
