5bac7a512cbd3bf2dfdf3216f75d602b0630daa4
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-05-13 17:29:40 +0200

    First approach to LoadCashflowByGivenPeriod method, without tests

--- BalanceCreator.h
+++ BalanceCreator.h
@@ -5,15 +5,30 @@
 #include <vector>
 
 #include "Cashflow.h"
+#include "CashflowFile.h"
+#include "PeriodServer.h"
 
 
 class BalanceCreator{
 public:
-    const std::string BALANCE_PERIOD;
-    BalanceCreator(std::string balancePeriod) : BALANCE_PERIOD(balancePeriod){}
+    std::string balancePeriod;
+    const int SIGNED_IN_USER_ID;
+    BalanceCreator(int signedInUserId, std::string incomeFilename, std::string expenseFilename)
+    : SIGNED_IN_USER_ID(signedInUserId)
+    , incomesFile(incomeFilename)
+    , expensesFile(expenseFilename)
+    {
+        PeriodServer periodServer;
+        balancePeriod = periodServer.specifyPeriod();
+    }
     void showBalanceByGivenPeriod();
-    std::string setPeriod();
 private:
+    std::vector<Cashflow> incomes;
+    std::vector<Cashflow> expenses;
+
+    CashflowFile incomesFile;
+    CashflowFile expensesFile;
+
     void sortBalanceByDateOldest();
     void showCashflowTable();
 
@@ -21,7 +36,8 @@ private:
     float calculateIncome();
     float calculateExpense();
 
-    std::vector<Cashflow> loadCasfhlowsFromFileByGivenPeriod();
+    void loadCasfhlowsFromFileByGivenPeriod();
+    bool isDate1Older( std::string , std::string );
 };
 
 #endif // BALANCE_CREATOR_H
