4594698c279db3181faa9bb5aa0596efb44d4b31
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-06-08 16:51:50 +0200

    Filled headers from UML

---
+++ TextMenu.h
@@ -0,0 +1,14 @@
+#ifndef TEXT_MENU_H
+#define TEXT_MENU_H
+
+#include <iostream>
+
+class TextMenu{
+public:
+    void showMainMenu();
+    void showSubmenuFinanceManager();
+    char choseFromMainMenu();
+    char choseFromSubmenuFinanceManager();
+private:
+};
+#endif // TEXT_MENU_H
