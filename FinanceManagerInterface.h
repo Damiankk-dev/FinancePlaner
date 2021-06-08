bf8c60a23531112d11934ed3fe7e925d690c195e
Damiankk-dev <damian.kaczmarzyk.programista@gmail.com>
2021-06-08 18:17:23 +0200

    Added methods declarations from UML

--- FinanceManagerInterface.h
+++ FinanceManagerInterface.h
@@ -12,6 +12,11 @@ class FinanceManagerInterface{
     FinanceOrganizer *financeOrganizer;
     const std::string INCOMES_FILENAME;
     const std::string EXPENSES_FILENAME;
+
+    void showMainMenu();
+    void showSubmenuSignedInUser();
+    bool isUserRegistered();
+
 public:
     char mainMenuChoice;
     FinanceManagerInterface( std::string registeredUsersFilename, std::string incomesFilename, std::string expensesFilename)
@@ -25,5 +30,14 @@ public:
         delete financeOrganizer;
         financeOrganizer = NULL;
     }
+    void registerUser();
+    void signInUser();
+    void signOutUser();
+    void changeUserPassword();
+    void addIncome();
+    void addExpense();
+    void createBalanceCurrentMonth();
+    void createBalancePastMonth();
+    void createBalanceAnyPeriod();
 };
 #endif // FINANCE_MANAGER_INTERFACE_H
